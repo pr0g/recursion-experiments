5127b56c4d228686d69978b7ba90e4737bf8555b
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-07-05 21:39:30 +0100

    add recursive implementation of binary search

---
+++ binary-search.cpp
@@ -0,0 +1,43 @@
+#include <format>
+#include <iostream>
+#include <span>
+
+std::optional<int> binary_search_recursive(
+  int needle, std::span<const int> haystack,
+  std::optional<int> left = std::nullopt,
+  std::optional<int> right = std::nullopt) {
+  if (left == std::nullopt) {
+    left = 0;
+  }
+  if (right == std::nullopt) {
+    right = haystack.size() - 1;
+  }
+  if (*left > *right) {
+    return std::nullopt;
+  }
+  std::cout << "searching: [";
+  for (int i = *left; i < *right; i++) {
+    std::cout << haystack[i] << ", ";
+  }
+  std::cout << haystack[*right] << "]\n";
+  int mid = *left + (*right - *left) / 2;
+  if (needle == haystack[mid]) {
+    return mid;
+  } else if (needle < haystack[mid]) {
+    return binary_search_recursive(needle, haystack, left, mid - 1);
+  } else /*if (needle > haystack[mid]) */ {
+    return binary_search_recursive(needle, haystack, mid + 1, right);
+  }
+}
+
+int main(int argc, char** argv) {
+
+  const int needle = 19;
+  const auto found =
+    binary_search_recursive(needle, {{1, 4, 8, 11, 13, 16, 19, 19}});
+  std::cout << std::format(
+    "position of {} in [1, 4, 8, 11, 13, 16, 19, 19] is {}\n", needle,
+    found.value_or(-1));
+
+  return 0;
+}

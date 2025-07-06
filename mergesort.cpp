4c7ab0181c30d467c7791bf7f83f969ddedb4fed
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-07-06 22:27:28 +0100

    add iterative recursive version of mergesort

--- mergesort.cpp
+++ mergesort.cpp
@@ -36,12 +36,75 @@ std::vector<int> mergesort_recursive(std::span<const int> items) {
   return merged;
 }
 
+std::vector<int> mergesort_iterative_recursive(std::span<const int> items) {
+  enum class return_address_e { before, recursive1, recursive2 };
+  struct frame_t {
+    return_address_e return_address;
+    std::span<const int> items;
+    std::vector<int> lhs;
+    std::vector<int> rhs;
+    int half;
+  };
+  std::stack<frame_t> call_stack;
+  call_stack.push(
+    frame_t{.return_address = return_address_e::before, .items = items});
+
+  std::vector<int> return_value;
+  while (!call_stack.empty()) {
+    auto& top = call_stack.top();
+    if (top.return_address == return_address_e::before) {
+      if (top.items.size() <= 1) {
+        return_value = std::vector<int>{top.items[0]};
+        call_stack.pop();
+        continue;
+      }
+      top.return_address = return_address_e::recursive1;
+      top.half = top.items.size() / 2;
+      call_stack.push(
+        frame_t{
+          .return_address = return_address_e::before,
+          .items = top.items.subspan(0, top.half)});
+    } else if (top.return_address == return_address_e::recursive1) {
+      top.return_address = return_address_e::recursive2;
+      top.lhs = return_value;
+      call_stack.push(
+        frame_t{
+          .return_address = return_address_e::before,
+          .items = top.items.subspan(top.half)});
+    } else if (top.return_address == return_address_e::recursive2) {
+      top.rhs = return_value;
+      std::vector<int> merged;
+      int l = 0, r = 0;
+      for (; l < top.lhs.size() && r < top.rhs.size();) {
+        if (top.lhs[l] < top.rhs[r]) {
+          merged.push_back(top.lhs[l++]);
+        } else {
+          merged.push_back(top.rhs[r++]);
+        }
+      }
+      for (; l < top.lhs.size(); l++) {
+        merged.push_back(top.lhs[l]);
+      }
+      for (; r < top.rhs.size(); r++) {
+        merged.push_back(top.rhs[r]);
+      }
+      return_value = merged;
+      call_stack.pop();
+    }
+  }
+  return return_value;
+}
+
 int main(int argc, char** argv) {
   std::vector<int> items = {2, 6, 1, 5, 10, 15, 11, 3, 9, 20, 8};
 
   std::cout << "recursive mergesort: \n";
-  const auto sorted = mergesort_recursive(items);
-  print_items(sorted);
+  const auto sorted_recursive = mergesort_recursive(items);
+  print_items(sorted_recursive);
+
+  std::cout << "iterative recursive mergesort: \n";
+  const auto iterative_recursive_sorted = mergesort_iterative_recursive(items);
+  print_items(iterative_recursive_sorted);
 
   return 0;
 }

7484047b1ed5aec5331f9b032d8ed8bff4dc9209
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-07-07 23:02:42 +0100

    initial implementation of recursive permutations

---
+++ permutations.cpp
@@ -0,0 +1,46 @@
+#include <format>
+#include <iostream>
+#include <span>
+
+std::vector<std::string> get_permutations(
+  const std::string& chars, int indent = 0) {
+  std::cout << std::format(
+    "{}Start of get_permutations ({})\n", std::string(indent, '.'), chars);
+  if (chars.size() == 1) {
+    std::cout << std::format(
+      "{}When chars = ({}), base case returns\n", std::string(indent, '.'),
+      chars);
+    return std::vector<std::string>(1, std::string(chars.begin(), chars.end()));
+  }
+  std::vector<std::string> permutations;
+  const auto head = chars[0];
+  const auto tail = chars.substr(1);
+  const auto tail_permutations = get_permutations(tail, indent + 1);
+  for (const auto tail_permutation : tail_permutations) {
+    std::cout << std::format(
+      "{}When chars = ({}), putting head {} in all places in {}\n",
+      std::string(indent, '.'), chars, head, tail);
+    for (int i = 0; i < tail_permutation.size() + 1; i++) {
+      const auto new_permutation =
+        tail_permutation.substr(0, i) + head + tail_permutation.substr(i);
+      std::cout << std::format(
+        "{}New permutation ({})\n", std::string(indent, '.'), new_permutation);
+      permutations.push_back(new_permutation);
+    }
+  }
+  return permutations;
+}
+
+int main(int argc, char** argv) {
+  const auto letters = std::string{"ABCD"};
+  const auto permutations = get_permutations(letters);
+
+  std::cout << '\n';
+
+  std::cout << "recursive permutations:\n";
+  for (const auto permutation : permutations) {
+    std::cout << permutation << '\n';
+  }
+
+  return 0;
+}

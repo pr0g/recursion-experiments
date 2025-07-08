1fb08acb2b942143bfd843b3f420b0c7d62b6d2b
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-07-08 11:12:23 +0100

    tidy-up naming in get permutations

--- permutations.cpp
+++ permutations.cpp
@@ -3,27 +3,27 @@
 #include <span>
 
 std::vector<std::string> get_permutations(
-  const std::string& chars, int indent = 0) {
-  const auto prefix = std::string(indent, '.');
-  std::cout << std::format("{}Start of get_permutations ({})\n", prefix, chars);
+  const std::string& chars, int indent_length = 0) {
+  const auto indent = std::string(indent_length, '.');
+  std::cout << std::format("{}Start of get_permutations ({})\n", indent, chars);
   if (chars.size() == 1) {
     std::cout << std::format(
-      "{}When chars = ({}), base case returns\n", prefix, chars);
+      "{}When chars = ({}), base case returns\n", indent, chars);
     return std::vector<std::string>(1, std::string(chars.begin(), chars.end()));
   }
   std::vector<std::string> permutations;
   const auto head = chars[0];
   const auto tail = chars.substr(1);
-  const auto tail_permutations = get_permutations(tail, indent + 1);
+  const auto tail_permutations = get_permutations(tail, indent_length + 1);
   for (const auto tail_permutation : tail_permutations) {
     std::cout << std::format(
-      "{}When chars = ({}), putting head {} in all places in {}\n", , chars,
-      head, tail);
+      "{}When chars = ({}), putting head {} in all places in {}\n", indent,
+      chars, head, tail);
     for (int i = 0; i < tail_permutation.size() + 1; i++) {
       const auto new_permutation =
         tail_permutation.substr(0, i) + head + tail_permutation.substr(i);
       std::cout << std::format(
-        "{}New permutation ({})\n", prefix, new_permutation);
+        "{}New permutation ({})\n", indent, new_permutation);
       permutations.push_back(new_permutation);
     }
   }

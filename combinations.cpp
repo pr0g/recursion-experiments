ad90514658972a8ee2b8a8cc38cf299f87142430
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-07-09 21:52:30 +0100

    add combinations function for arbitrary type

--- combinations.cpp
+++ combinations.cpp
@@ -1,7 +1,8 @@
 #include <format>
 #include <iostream>
+#include <ranges>
 
-std::vector<std::string> get_combinations_logging(
+std::vector<std::string> get_combinations_recursive_logging(
   const std::string& characters, int k, int indent_count = 0) {
   std::string indent(indent_count, '.');
   const std::string debug_message =
@@ -20,7 +21,7 @@ std::vector<std::string> get_combinations_logging(
   std::cout << std::format(
     "{} part 1, get combinations with head \"{}\"\n", debug_message, head);
   const auto tail_combinations =
-    get_combinations_logging(tail, k - 1, indent_count + 1);
+    get_combinations_recursive_logging(tail, k - 1, indent_count + 1);
   std::cout << std::format(
     "{}Adding head \"{}\" to tail combinations\n", indent, head);
   for (const auto& tail_combination : tail_combinations) {
@@ -31,7 +32,7 @@ std::vector<std::string> get_combinations_logging(
   std::cout << std::format(
     "{} part 2, get combinations without head \"{}\"\n", debug_message, head);
   const auto next_combinations =
-    get_combinations_logging(tail, k, indent_count + 1);
+    get_combinations_recursive_logging(tail, k, indent_count + 1);
   combinations.insert(
     combinations.end(), next_combinations.begin(), next_combinations.end());
   std::cout << std::format("{} results are: ", debug_message);
@@ -42,8 +43,8 @@ std::vector<std::string> get_combinations_logging(
   return combinations;
 }
 
-std::vector<std::string> get_combinations(
-  const std::string& characters, int k, int indent_count = 0) {
+std::vector<std::string> get_combinations_recursive(
+  const std::string& characters, int k) {
   if (k == 0) {
     return std::vector<std::string>({""});
   } else if (characters == "") {
@@ -52,12 +53,35 @@ std::vector<std::string> get_combinations(
   std::vector<std::string> combinations;
   const std::string head = characters.substr(0, 1);
   const std::string tail = characters.substr(1);
-  const auto tail_combinations =
-    get_combinations(tail, k - 1, indent_count + 1);
+  const auto tail_combinations = get_combinations_recursive(tail, k - 1);
   for (const auto& tail_combination : tail_combinations) {
     combinations.push_back(head + tail_combination);
   }
-  const auto next_combinations = get_combinations(tail, k, indent_count + 1);
+  const auto next_combinations = get_combinations_recursive(tail, k);
+  combinations.insert(
+    combinations.end(), next_combinations.begin(), next_combinations.end());
+  return combinations;
+}
+
+template<typename T>
+std::vector<std::vector<T>> get_combinations_any_recursive(
+  const std::vector<T>& values, int k) {
+  if (k == 0) {
+    return std::vector<std::vector<T>>(1, std::vector<T>());
+  } else if (values.empty()) {
+    return std::vector<std::vector<T>>();
+  }
+  std::vector<std::vector<T>> combinations;
+  const auto head_view = values | std::views::take(1);
+  const auto tail_view = values | std::views::drop(1);
+  const auto tail = std::vector<T>(tail_view.begin(), tail_view.end());
+  const auto tail_combinations = get_combinations_any_recursive(tail, k - 1);
+  for (const auto& tail_combination : tail_combinations) {
+    std::vector<T> next(head_view.begin(), head_view.end());
+    next.insert(next.end(), tail_combination.begin(), tail_combination.end());
+    combinations.push_back(next);
+  }
+  const auto next_combinations = get_combinations_any_recursive(tail, k);
   combinations.insert(
     combinations.end(), next_combinations.begin(), next_combinations.end());
   return combinations;
@@ -65,7 +89,7 @@ std::vector<std::string> get_combinations(
 
 int main(int argc, char** argv) {
   {
-    const auto combinations = get_combinations("ABC", 2);
+    const auto combinations = get_combinations_recursive("ABC", 2);
     std::cout << "recursive combinations:\n";
     for (const auto& combination : combinations) {
       std::cout << std::format("{}\n", combination);
@@ -73,7 +97,7 @@ int main(int argc, char** argv) {
   }
   std::cout << "\n";
   {
-    const auto combinations = get_combinations("ABC", 3);
+    const auto combinations = get_combinations_recursive("ABC", 3);
     std::cout << "recursive combinations:\n";
     for (const auto& combination : combinations) {
       std::cout << std::format("{}\n", combination);
@@ -81,7 +105,7 @@ int main(int argc, char** argv) {
   }
   std::cout << "\n";
   {
-    const auto combinations = get_combinations("ABC", 1);
+    const auto combinations = get_combinations_recursive("ABC", 1);
     std::cout << "recursive combinations:\n";
     for (const auto& combination : combinations) {
       std::cout << std::format("{}\n", combination);
@@ -89,10 +113,22 @@ int main(int argc, char** argv) {
   }
   std::cout << "\n";
   {
-    const auto combinations = get_combinations("ABCDE", 3);
+    const auto combinations = get_combinations_recursive("ABCDE", 3);
     std::cout << "recursive combinations:\n";
     for (const auto& combination : combinations) {
       std::cout << std::format("{}\n", combination);
+    }
+  }
+  std::cout << "\n";
+  {
+    const auto combinations =
+      get_combinations_any_recursive(std::vector<int>{1, 2, 3, 4, 5}, 3);
+    std::cout << "recursive combinations:\n";
+    for (const auto& combination : combinations) {
+      for (auto v : combination | std::views::take(combination.size() - 1)) {
+        std::cout << std::format("{},", v);
+      }
+      std::cout << combination[combination.size() - 1] << '\n';
     }
   }
   return 0;

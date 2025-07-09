ba582f25142272316456679073f9b2c19034c906
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-07-09 16:19:01 +0100

    add power_set iterative recursive function

--- power-set.cpp
+++ power-set.cpp
@@ -15,11 +15,11 @@ std::vector<std::string> get_power_set_recursive(
   if (characters.empty()) {
     return std::vector<std::string>(1, "");
   }
-  std::vector<std::string> power_set;
   const auto head = characters[0];
   const auto tail = characters.substr(1);
   // get the sets that don't include the head
   const auto tail_power_set = get_power_set_recursive(tail);
+  std::vector<std::string> power_set;
   // get the sets that include the head
   for (const auto& tail_set : tail_power_set) {
     power_set.push_back(head + tail_set);
@@ -29,11 +29,63 @@ std::vector<std::string> get_power_set_recursive(
   return power_set;
 }
 
+std::vector<std::string> get_power_set_iterative_recursive(
+  const std::string& characters) {
+  enum class return_address_e { before, recursive };
+  struct frame_t {
+    return_address_e return_address;
+    std::string characters;
+    std::string head;
+  };
+  std::stack<frame_t> call_stack;
+  call_stack.push(
+    frame_t{
+      .return_address = return_address_e::before, .characters = characters});
+  std::vector<std::string> return_value;
+  while (!call_stack.empty()) {
+    auto& top = call_stack.top();
+    if (top.return_address == return_address_e::before) {
+      if (top.characters.empty()) {
+        return_value = std::vector<std::string>(1, "");
+        call_stack.pop();
+        continue;
+      }
+      top.return_address = return_address_e::recursive;
+      top.head = top.characters[0];
+      call_stack.push(
+        frame_t{
+          .characters = top.characters.substr(1),
+          .return_address = return_address_e::before});
+    } else if (top.return_address == return_address_e::recursive) {
+      const auto tail_power_set = return_value;
+      std::vector<std::string> power_set;
+      // get the sets that include the head
+      for (const auto& tail_set : tail_power_set) {
+        power_set.push_back(top.head + tail_set);
+      }
+      power_set.insert(
+        power_set.end(), tail_power_set.begin(), tail_power_set.end());
+      return_value = power_set;
+      call_stack.pop();
+    }
+  }
+  return return_value;
+}
+
 int main(int argc, char** argv) {
-  std::cout << "recursive power set:\n";
-  const auto power_set = get_power_set_recursive("ABC");
-  for (const auto& set : power_set) {
-    std::cout << set << '\n';
+  {
+    std::cout << "recursive power set:\n";
+    const auto power_set = get_power_set_recursive("ABC");
+    for (const auto& set : power_set) {
+      std::cout << set << '\n';
+    }
+  }
+  {
+    std::cout << "iterative power set:\n";
+    const auto power_set = get_power_set_iterative_recursive("ABC");
+    for (const auto& set : power_set) {
+      std::cout << set << '\n';
+    }
   }
   return 0;
 }

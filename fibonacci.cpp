67a3abf83b342ceda0f00b5bfa6c9bc4e4a86bf9
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-07-10 11:08:44 +0100

    add iterative recursive memoized version of fibonacci

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -92,25 +92,74 @@ int fibonacci_recursive_memoized(int n) {
   return fibonacci_recursive_memoized_internal(n, cache);
 }
 
+int fibonacci_iterative_recursive_memoized(const int n) {
+  enum class return_address_e { before, recursive1, recursive2 };
+  struct frame_t {
+    return_address_e return_address;
+    int n;
+    int minus1;
+    int minus2;
+  };
+  std::unordered_map<int, int> cache;
+  std::stack<frame_t> call_stack;
+  call_stack.push(frame_t{.return_address = return_address_e::before, .n = n});
+  int return_value = 0;
+  while (!call_stack.empty()) {
+    auto& top = call_stack.top();
+    if (top.return_address == return_address_e::before) {
+      if (top.n <= 1) {
+        return_value = top.n;
+        call_stack.pop();
+        continue;
+      }
+      if (auto it = cache.find(top.n); it != cache.end()) {
+        return_value = it->second;
+        call_stack.pop();
+        continue;
+      }
+      top.return_address = return_address_e::recursive1;
+      call_stack.push(
+        frame_t{.return_address = return_address_e::before, .n = top.n - 1});
+    } else if (top.return_address == return_address_e::recursive1) {
+      top.return_address = return_address_e::recursive2;
+      top.minus1 = return_value;
+      call_stack.push(
+        frame_t{.return_address = return_address_e::before, .n = top.n - 2});
+    } else if (top.return_address == return_address_e::recursive2) {
+      top.minus2 = return_value;
+      return_value = top.minus1 + top.minus2;
+      cache.insert({top.n, return_value});
+      call_stack.pop();
+    }
+  }
+  return return_value;
+}
+
 int main(int argc, char** argv) {
   std::cout << "fibonacci iterative:\n";
-  for (int i = 0; i < 20; i++) {
+  for (int i = 0; i <= 20; i++) {
     std::cout << std::format("{} is {}\n", i, fibonacci_iterative(i));
   }
   std::cout << '\n';
   std::cout << "fibonacci recursive:\n";
-  for (int i = 0; i < 20; i++) {
+  for (int i = 0; i <= 20; i++) {
     std::cout << std::format("{} is {}\n", i, fibonacci_recursive(i));
   }
   std::cout << '\n';
   std::cout << "fibonacci iterative recursive:\n";
-  for (int i = 0; i < 20; i++) {
+  for (int i = 0; i <= 20; i++) {
     std::cout << std::format("{} is {}\n", i, fibonacci_iterative_recursive(i));
   }
   std::cout << '\n';
   std::cout << "fibonacci recursive memoized:\n";
-  for (int i = 0; i < 20; i++) {
+  for (int i = 0; i <= 20; i++) {
     std::cout << std::format("{} is {}\n", i, fibonacci_recursive_memoized(i));
+  }
+  std::cout << '\n';
+  std::cout << "fibonacci iterative recursive memoized:\n";
+  for (int i = 0; i <= 20; i++) {
+    std::cout << std::format(
+      "{} is {}\n", i, fibonacci_iterative_recursive_memoized(i));
   }
   return 0;
 }

d8fa61422d32422e3035b5c6403971dac1dbbc0e
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-07-12 20:29:38 +0100

    add sum series iterative recursive

--- sums.cpp
+++ sums.cpp
@@ -1,5 +1,6 @@
 #include <format>
 #include <iostream>
+#include <stack>
 
 int sum_series_iterative(int n) {
   int result = 0;
@@ -14,6 +15,34 @@ int sum_series_recursive(int n) {
     return 1;
   }
   return n + sum_series_recursive(n - 1);
+}
+
+int sum_series_iterative_recursive(int n) {
+  enum class return_address_e { before, recursive };
+  struct frame_t {
+    return_address_e return_address;
+    int n;
+  };
+  std::stack<frame_t> call_stack;
+  call_stack.push(frame_t{.return_address = return_address_e::before, .n = n});
+  int return_value;
+  while (!call_stack.empty()) {
+    auto& top = call_stack.top();
+    if (top.return_address == return_address_e::before) {
+      if (top.n == 1) {
+        return_value = 1;
+        call_stack.pop();
+        continue;
+      }
+      top.return_address = return_address_e::recursive;
+      call_stack.push(
+        frame_t{.return_address = return_address_e::before, .n = top.n - 1});
+    } else if (top.return_address == return_address_e::recursive) {
+      return_value = top.n + return_value;
+      call_stack.pop();
+    }
+  }
+  return return_value;
 }
 
 int power_series_iterative(int n) {
@@ -34,26 +63,37 @@ int power_series_recursive(int n, int power = 2) {
 }
 
 int main(int argc, char** argv) {
+  std::cout << std::format("sum series iterative:\n");
   for (int i = 0; i < 5; i++) {
     std::cout << std::format(
       "sum of numbers up to {} is {}\n", i + 1, sum_series_iterative(i + 1));
   }
-  std::cout << "---\n";
+  std::cout << '\n';
+  std::cout << std::format("sum series recursive:\n");
   for (int i = 0; i < 5; i++) {
     std::cout << std::format(
       "sum of numbers up to {} is {}\n", i + 1, sum_series_recursive(i + 1));
   }
-  std::cout << "---\n";
+  std::cout << '\n';
+  std::cout << std::format("power series iterative:\n");
   for (int i = 0; i < 5; i++) {
     std::cout << std::format(
       "sum of power of 2 numbers up to {} is {}\n", i + 1,
       power_series_iterative(i + 1));
   }
-  std::cout << "---\n";
+  std::cout << '\n';
+  std::cout << std::format("power series recursive:\n");
   for (int i = 0; i < 5; i++) {
     std::cout << std::format(
       "sum of power of 2 numbers up to {} is {}\n", i + 1,
       power_series_recursive(i + 1));
+  }
+  std::cout << '\n';
+  std::cout << std::format("sum series iterative recursive:\n");
+  for (int i = 0; i < 5; i++) {
+    std::cout << std::format(
+      "sum of power of 2 numbers up to {} is {}\n", i + 1,
+      sum_series_iterative_recursive(i + 1));
   }
   return 0;
 }

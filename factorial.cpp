08026209732e88b3ac49b9e08f11d378c2dd9d9c
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2025-07-17 11:46:55 +0100

    further work walking through iterative recursive factorial function

--- factorial.cpp
+++ factorial.cpp
@@ -37,33 +37,40 @@ int factorial_iterative_3(int n) {
 // first half is factorial_recursive(number - 1)
 // second half is <number> * <return value> (e.g. 5 * 24)
 
-int factorial_recursive(int n) {
+int factorial_recursive_1(int n) {
   if (n <= 1) {
     return 1;
   }
-  return n * factorial_recursive(n - 1);
+  return n * factorial_recursive_1(n - 1);
+}
+
+int factorial_recursive_2(int n) {
+  if (n <= 1) {
+    return 1;
+  }
+  int n_minus_1 = factorial_recursive_2(n - 1);
+  return n * n_minus_1;
 }
 
 int factorial_recursive_iterative(int n) {
-  enum class return_address_e { before, recursive };
+  enum class return_address_e { recursive };
   struct frame_t {
-    return_address_e return_address;
+    std::optional<return_address_e> return_address;
     int n;
   };
   std::stack<frame_t> call_stack;
-  call_stack.push(frame_t{.return_address = return_address_e::before, .n = n});
+  call_stack.push(frame_t{.return_address = std::nullopt, .n = n});
   int return_value; // uninitialised
-  while (call_stack.size() > 0) {
+  while (!call_stack.empty()) {
     auto& top = call_stack.top();
-    if (top.return_address == return_address_e::before) {
+    if (!top.return_address.has_value()) {
       if (top.n <= 1) {
         return_value = 1;
         call_stack.pop();
-      } else {
-        top.return_address = return_address_e::recursive;
-        call_stack.push(
-          frame_t{.return_address = return_address_e::before, .n = top.n - 1});
+        continue;
       }
+      top.return_address = return_address_e::recursive;
+      call_stack.push(frame_t{.n = top.n - 1});
     } else if (top.return_address == return_address_e::recursive) {
       return_value = top.n * return_value;
       call_stack.pop();
@@ -89,9 +96,14 @@ int main(int argc, char** argv) {
     std::cout << std::format("{}! is {}\n", i, factorial_iterative_3(i));
   }
   std::cout << '\n';
-  std::cout << "factorial recursive:\n";
+  std::cout << "factorial recursive 1:\n";
   for (int i = 0; i <= count; i++) {
-    std::cout << std::format("{}! is {}\n", i, factorial_recursive(i));
+    std::cout << std::format("{}! is {}\n", i, factorial_recursive_1(i));
+  }
+  std::cout << '\n';
+  std::cout << "factorial recursive 2:\n";
+  for (int i = 0; i <= count; i++) {
+    std::cout << std::format("{}! is {}\n", i, factorial_recursive_2(i));
   }
   std::cout << '\n';
   std::cout << "factorial iterative recursive:\n";
